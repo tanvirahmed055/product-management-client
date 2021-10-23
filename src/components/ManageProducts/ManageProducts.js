d643539a884b07c7b29c7d11bf25d899677439ea
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-23 22:33:43 +0600

    implemented task 5

--- src/components/ManageProducts/ManageProducts.js
+++ src/components/ManageProducts/ManageProducts.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ManageProducts = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/products')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, [])
     return (
         <div>
-
+            <h2>All Products</h2>
+            {
+                products.map(product => <li>Name:{product?.name} Price:{product?.price} Quantity: {product?.quantity}</li>)
+            }
         </div>
     );
 };
