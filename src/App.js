3f62570dd0112faa608d109d1c71f083ddac4651
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-23 20:29:05 +0600

    implemented task 3 & 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Home from "./components/Home/Home";
+import Header from './components/Header/Header';
+import AddProducts from "./components/AddProducts/AddProducts";
+import ManageProducts from "./components/ManageProducts/ManageProducts";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route exact path="/manageProducts">
+              <ManageProducts></ManageProducts>
+            </Route>
+            <Route path="/addProduct">
+              <AddProducts></AddProducts>
+            </Route>
+
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/AddProducts/AddProducts.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+
+
+
+const AddProducts = () => {
+    const [user, setUser] = useState({});
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        // console.log(data)
+        const newUser = {
+            name: data?.name,
+            price: data?.price,
+            quantity: data?.quantity
+        };
+        setUser(newUser);
+        console.log(user);
+
+        const url = "http://localhost:3001/add";
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {/* register your input into the hook by invoking the "register" function */}
+                <input defaultValue="test" {...register("name")} />
+
+                <input defaultValue="test" {...register("price")} />
+
+                <input defaultValue="test" {...register("quantity")} />
+
+
+
+
+                {/* errors will return when field validation fails  */}
+                {errors.exampleRequired && <span>This field is required</span>}
+
+                <input type="submit" />
+            </form>
+        </div>
+    );
+};
+
+export default AddProducts;
