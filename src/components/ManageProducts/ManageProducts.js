9eb36a8ca6985db279b46f83a79565f0ad4cfd52
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-24 00:10:23 +0600

    implemented task 6

--- src/components/ManageProducts/ManageProducts.js
+++ src/components/ManageProducts/ManageProducts.js
@@ -8,11 +8,20 @@ const ManageProducts = () => {
             .then(res => res.json())
             .then(data => setProducts(data));
     }, [])
+
+    const handleDelete = async (id) => {
+
+        const url = `http://localhost:3001/users/${id}`;
+        fetch(url, {
+            method: 'DELETE',
+        })
+    }
     return (
         <div>
             <h2>All Products</h2>
             {
-                products.map(product => <li>Name:{product?.name} Price:{product?.price} Quantity: {product?.quantity}</li>)
+                products.map(product => <li>Name:{product?.name} Price:{product?.price} Quantity: {product?.quantity}<button onClick={() => { handleDelete(product._id) }}>delete</button>
+                    {product._id}</li>)
             }
         </div>
     );
