fa11032cd412dd1f8384d0948e3690e44aec679a
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-24 01:00:43 +0600

    implemented task 8

--- src/components/UpdateProduct/UpdateProduct.js
+++ src/components/UpdateProduct/UpdateProduct.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 
 const UpdateProduct = () => {
     const { id } = useParams();
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/products/${id}`)
+            .then(res => res.json())
+            .then(data => setProduct(data))
+    }, [])
     return (
         <div>
             <h2>Welcome to update product</h2>
             <p>{id}</p>
+
+            <h2>Product ID: {product?._id}</h2>
+            <h2>Product name: {product?.name}</h2>
+            <h2>Product price: {product?.price}</h2>
+            <h2>Product quantity: {product?.quantity}</h2>
+
         </div>
     );
 };
